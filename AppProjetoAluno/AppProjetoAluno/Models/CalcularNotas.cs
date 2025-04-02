7be2b796e3ca3db18712ad90f308d42acd187053
Luan Mateus <luanmateus2002@gmail.com>
2025-04-01 21:54:02 -0400

    ok

---
+++ AppProjetoAluno/AppProjetoAluno/Models/CalcularNotas.cs
@@ -0,0 +1,24 @@
+﻿namespace AppProjetoAluno.Models
+{
+    public class CalcularNotas
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Media { get; set; }
+
+        public double CalcularMedia()
+        {
+           return(Nota1 + Nota2) /2;
+        }
+        public string? Situacao()
+        {
+           string? result = null;
+            if(CalcularMedia() <6) result = "Reprovado";
+            if (CalcularMedia() > 6) result = "Aprovado";
+            return result; 
+        }
+
+    }
+}
---
+++ BlazorAppIMC/BlazorAppIMC/Models/CalcularNotas.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorAppIMC.Models
+{
+    public class CalcularNotas
+    {
+        public int id {  get; set; }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Media { get; set; }
+        public string? Situacao { get; set; }
+        public void CalcularMedia()
+        {
+            Situacao = "REPROVADO(A)";
+            Media = (Nota1 + Nota2) / 2;
+            if(Media >= 60)
+            {
+                Situacao = "APROVADO(A)";
+            }
+
+        }
+
+    }
+
+}
