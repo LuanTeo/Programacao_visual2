00e0e0dc1865cb4d099801417a9f9f635ca64c8e
Luan Mateus <106929115+LuanTeo@users.noreply.github.com>
2025-04-15 22:08:16 -0400

    Add files via upload

---
+++ AppInventario/AppInventario/Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace AppInventario.Models
+{
+    [Table("pessoa")]
+    public class Pessoa
+    {
+        [Column("i")]
+        public int? Id { get; set; }
+
+        [Column("nome")]
+        public string? Nome { get; set; }
+
+        [Column("cpf")]
+        public string? Cpf { get; set; }
+
+        [Column("data_nascimento")]
+        public DateTime? DataNasc { get; set; }
+
+        [Column("telefone")]
+        public string? Telefone { get; set; }
+        
+        public List<Propriedade> Propriedade { get; set;}
+    }
+}
